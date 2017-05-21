1947619819616e30b1cdc7647e1c118b8f7a29cf
Mat Ruane <matr800@gmail.com>
2017-05-21 13:05:45 +1200

    initial implementation of node class

--- node.cpp
+++ node.cpp
@@ -1,2 +1,21 @@
 #include "node.h"
 
+class Node {
+private:
+    char ID;
+    char northNode;
+    char eastNode;
+    char southNode;
+    char westNode;
+    int paths;
+
+public:
+    Node(char id, char north, char east, char south, char west) {
+        ID = id;
+        northNode = north;
+        eastNode = east;
+        southNode = south;
+        westNode = west;
+    }
+    char getNeighbour(char direction);
+};
